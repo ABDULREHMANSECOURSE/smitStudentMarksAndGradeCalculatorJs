466e35249c5dfd37c46769b347a6a7888b3cab17
ABDUL REHMAN SE COURSE <ABDULREHMANSECOURSE@GMAIL.COM>
2025-09-23 01:50:42 +0500

    underconstruction

--- app.js
+++ app.js
@@ -12,16 +12,38 @@ function menuVisible(){
 
 menuIcon.onclick = menuVisible
 
+var subjectMarkMain = document.getElementById('subjectMarkMain')
+
+var subjectMark = document.getElementById('subjectMark')
 
 var subject = document.getElementById('subject');
 var mark = document.getElementById('mark')
 
 
+var addSubject = document.getElementById('addSubject')
 var button = document.getElementById('button');
 
+var result = document.getElementById('result')
+
+var resultSM = document.getElementById('resultSM')
 
 var resultSubject = document.getElementById('resultSubject');
 var resultMarks = document.getElementById('resultMarks')
+
+
+function subjectMarkAdder() {
+    var resultCopy = resultSM.cloneNode(true)
+    resultCopy.removeAttribute("id")
+    result.appendChild(resultCopy)
+
+    var copy = subjectMark.cloneNode(true);
+    copy.removeAttribute("id");
+    subjectMarkMain.appendChild(copy);
+}
+
+addSubject.onclick = subjectMarkAdder;
+
+
 
 
 function changeText() {
