3306316aa0dadc352e9184937869d338ef315532
ABDUL REHMAN SE COURSE <ABDULREHMANSECOURSE@GMAIL.COM>
2025-09-23 12:13:24 +0500

    /\

--- app.js
+++ app.js
@@ -1,54 +1,50 @@
-var nav = document.getElementById('nav')
-var menuIcon = document.getElementById('menuIcon')
-
-
-function menuVisible(){
-    if (nav.style.display === "flex") {
-        nav.style.display = "none"
-    } else {
-        nav.style.display = "flex"
-    }
-}
-
-menuIcon.onclick = menuVisible
-
-var subjectMarkMain = document.getElementById('subjectMarkMain')
-
-var subjectMark = document.getElementById('subjectMark')
-
-var subject = document.getElementById('subject');
-var mark = document.getElementById('mark')
-
-
-var addSubject = document.getElementById('addSubject')
+var nav = document.getElementById('nav');
+var menuIcon = document.getElementById('menuIcon');
+var subjectMarkMain = document.getElementById('subjectMarkMain');
+var subjectMark = document.getElementById('subjectMark');
+var addSubject = document.getElementById('addSubject');
 var button = document.getElementById('button');
+var result = document.getElementById('result');
+var resultSM = document.getElementById('resultSM');
 
-var result = document.getElementById('result')
-
-var resultSM = document.getElementById('resultSM')
-
-var resultSubject = document.getElementById('resultSubject');
-var resultMarks = document.getElementById('resultMarks')
-
+function menuVisible() {
+    nav.style.display = (nav.style.display === "flex") ? "none" : "flex";
+}
+menuIcon.onclick = menuVisible;
 
 function subjectMarkAdder() {
-    var resultCopy = resultSM.cloneNode(true)
-    resultCopy.removeAttribute("id")
-    result.appendChild(resultCopy)
+    var resultCopy = resultSM.cloneNode(true);
+    resultCopy.removeAttribute("id");
+
+    // ✅ result box khali karo
+    var resultSubject = resultCopy.querySelector("#resultSubject");
+    var resultMarks = resultCopy.querySelector("#resultMarks");
+    if (resultSubject) resultSubject.innerText = "";
+    if (resultMarks) resultMarks.innerText = "";
+
+    result.appendChild(resultCopy);
 
     var copy = subjectMark.cloneNode(true);
     copy.removeAttribute("id");
+
+    // ✅ input clear
+    var inputs = copy.querySelectorAll("input");
+    inputs.forEach(input => input.value = "");
+
     subjectMarkMain.appendChild(copy);
 }
-
 addSubject.onclick = subjectMarkAdder;
 
-
-
-
 function changeText() {
-    resultSubject.innerText = subject.value;
-    resultMarks.innerText = mark.value
-}
+    var allSubjects = subjectMarkMain.querySelectorAll("input[id='subject']");
+    var allMarks = subjectMarkMain.querySelectorAll("input[id='mark']");
 
+    var allResultSubjects = result.querySelectorAll("#resultSubject");
+    var allResultMarks = result.querySelectorAll("#resultMarks");
+
+    for (var i = 0; i < allSubjects.length; i++) {
+        allResultSubjects[i].innerText = allSubjects[i].value;
+        allResultMarks[i].innerText = allMarks[i].value;
+    }
+}
 button.onclick = changeText;
